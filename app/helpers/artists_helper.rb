18363b589acfb306a535d1b41a185be758802c0a
Justin Tooley <jstooley823@gmail.com>
2018-01-05 01:43:56 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,9 +1,9 @@
 module ArtistsHelper
   def display_artist(song)
-    if song.artist_name
+
       link_to_if song.artist_name, song.artist_name, song.artist
-    else
-      link_to 'Add Artist', edit_song_path(song)
-    end
+
+      link_to_unless song.artist_name, 'Add Artist', edit_song_path(song)
+
   end
 end
